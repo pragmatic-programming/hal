5eb8a0b9fd370600f65857f4f9841811b0d78020
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-10-10 16:30:56 +0200

    Add material-ui as dependency

--- src/renderer/Renderer.tsx
+++ src/renderer/Renderer.tsx
@@ -6,6 +6,7 @@ import HtmlRenderer from "./html/HtmlRenderer";
 import { Editor } from "../model/Editor";
 import { Dimension } from "../model/Dimension";
 import { Position } from "../model/Position";
+import { Button } from "@mui/material";
 
 interface Props {
     canvas: Canvas;
@@ -15,18 +16,22 @@ export default function Renderer(props: Props): JSX.Element {
     const [items, setItems] = useState(props.canvas);
     return (
         <div>
-            <button onClick={() => {
-                setItems(items.addEditor(
-                    new Editor(
-                        3,
-                        new Dimension(640, 480),
-                        new Position(0, 0),
-                        "javascript",
-                        "alert('Hello '+ x)"
-                    ),
-                ));
-            }}>Test
-            </button>
+            <Button
+                variant="contained"
+                onClick={() => {
+                    setItems(items.addEditor(
+                        new Editor(
+                            3,
+                            new Dimension(640, 480),
+                            new Position(0, 0),
+                            "javascript",
+                            "alert('Hello '+ x)"
+                        ),
+                    ));
+                }}
+            >
+                New Editor
+            </Button>;
             <div>
                 <HtmlRenderer editors={items.editors}/>
                 <SvgRenderer editors={items.editors}/>
