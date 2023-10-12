68ac8eb9a768398a4521d1c1fbf284a7a6b167c4
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-10-12 17:35:33 +0200

    Refactor EditorRenderer

--- src/renderer/html/EditorHeader.tsx
+++ src/renderer/html/EditorHeader.tsx
@@ -2,20 +2,22 @@ import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import { Typography } from "@mui/material";
 import React from "react";
 
-export function EditorHeader() {
-    return <div
-        style={{
-            display: "inline-block",
-            paddingTop: 5,
-            paddingBottom: 5,
-            paddingLeft: 10,
-            paddingRight: 10,
-            height: 20,
-            // todo should come from theme
-            backgroundColor: "#ffffff"
-        }}
-    >
-        <InsertDriveFileIcon fontSize="small"/>
-        <Typography style={{marginLeft: 25}} variant="caption">filename.js</Typography>
-    </div>;
+export function EditorHeader(): React.JSX.Element {
+    return (
+        <div
+            style={{
+                display: "inline-block",
+                paddingTop: 5,
+                paddingBottom: 5,
+                paddingLeft: 10,
+                paddingRight: 10,
+                height: 20,
+                // todo should come from theme
+                backgroundColor: "#ffffff"
+            }}
+        >
+            <InsertDriveFileIcon fontSize="small"/>
+            <Typography style={{marginLeft: 25}} variant="caption">filename.js</Typography>
+        </div>
+    );
 }
--- src/renderer/html/EditorRenderer.tsx
+++ src/renderer/html/EditorRenderer.tsx
@@ -1,56 +1,19 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import { Editor } from "../../model/Editor";
 import { Editor as Monaco } from "@monaco-editor/react";
-import { Theme, useTheme } from "@mui/material";
 import { useStore } from "../../Store";
 import { State } from "../../State";
-import { useDraggable } from "@dnd-kit/core";
 import { EditorHeader } from "./EditorHeader";
+import { Draggable } from "./Draggable";
 
 interface Props {
     editor: Editor;
 }
 
-//todo this components requires refactoring
 export function EditorRenderer(props: Props): React.JSX.Element {
-    const theme: Theme = useTheme();
-    const menuWidth: number = useStore((state: State) => state.menuWidth);
-    const firstSelectedEditor: number | null = useStore((state: State) => state.highlightedEditor.first);
-    const secondSelectedEditor: number | null = useStore((state: State) => state.highlightedEditor.second);
-    const selectEditor = useStore((state: State) => state.selectEditor);
     const updateEditorValue = useStore((state: State) => state.updateEditorValue);
-    const {listeners, setNodeRef, transform} = useDraggable({id: props.editor.id,});
-    let style: Partial<CSSProperties> = {
-        borderColor: theme.palette.info.light,
-        borderStyle: "solid",
-        borderWidth: 1,
-        background: theme.palette.gui.menu.background
-    };
-    if (transform) {
-        style = {
-            transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-        };
-    }
-    if (props.editor.id === firstSelectedEditor || props.editor.id === secondSelectedEditor) {
-        style = {
-            ...style,
-            borderWidth: 2
-        };
-    }
     return (
-        <div
-            ref={setNodeRef}
-            className={"rectangle"}
-            style={{
-                ...style,
-                left: props.editor.position.x + menuWidth,
-                top: props.editor.position.y,
-                height: props.editor.dimension.height,
-                width: props.editor.dimension.width,
-            }}
-            {...listeners}
-            onDoubleClick={() => selectEditor(props.editor.id)}
-        >
+        <Draggable editor={props.editor}>
             <EditorHeader/>
             <Monaco
                 defaultLanguage={props.editor.language}
@@ -64,6 +27,6 @@ export function EditorRenderer(props: Props): React.JSX.Element {
                 }}
                 onChange={(value) => updateEditorValue(props.editor.id, value)}
             />
-        </div>
+        </Draggable>
     );
 }
