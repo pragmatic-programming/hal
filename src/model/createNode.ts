b8b9b627f5f2aec16fff6c4b963b1c29605feac5
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-10-24 18:10:34 +0300

    Create nodes based on incoming edges

--- src/model/createNode.ts
+++ src/model/createNode.ts
@@ -1,9 +1,13 @@
-import { SourceNode } from "ihgraph";
+import { SourceNode, TransformationEdge } from "ihgraph";
+import { Node } from "reactflow";
 
-export function createNode(sourceNode: SourceNode) {
+export function createNode(sourceNode: SourceNode): Node {
+    const executeEdge = sourceNode
+        .getIncomingEdges()
+        .find((edge: TransformationEdge) => edge.getType().getId() === "execute");
     return {
         id: sourceNode.getId(),
-        type: "editorNode",
+        type: executeEdge ? "resultNode" : "editorNode",
         data: {value: sourceNode.getContent()},
         // will be changed by automatic layout
         position: {x: 0, y: 0},
