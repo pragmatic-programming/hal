750b9463458a7f25547d3fdc201d6ad156bb39a3
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-10-26 10:57:59 +0300

    Add tooltips to bottom view

--- src/ui/bottom/Model.tsx
+++ src/ui/bottom/Model.tsx
@@ -1,4 +1,4 @@
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, IconButton, Tooltip } from "@mui/material";
 import AccountTreeIcon from "@mui/icons-material/AccountTree";
 import React from "react";
 import { useStore } from "../../state/Store";
@@ -13,51 +13,60 @@ interface Props {
 }
 
 export default function Model(props: Props): React.JSX.Element {
-    const {getNode, fitView } = useReactFlow();
+    const {getNode, fitView} = useReactFlow();
     const renderIHGraph = useStore((state: State) => state.renderIhGraph);
     let borderStyle: "dotted" | "solid" | "double";
     let borderWidth: number;
     let marginLeft: "-10px" | "0px" = "0px";
     let marginRight: "-10px" | "0px" = "0px";
     let property: IHGraph = props.processor.environment.getResult();
+    let title : string;
     switch (props.position) {
         case "start":
             borderStyle = "dotted";
             borderWidth = 2;
             marginLeft = "-10px";
             property = props.processor.environment.getProperty(Environment.SOURCE_MODEL);
+            title = "Input"
             break;
         case "inter":
             borderStyle = "solid";
             borderWidth = 2;
+            title = "Intermediate"
             break;
         case "end":
             borderStyle = "double";
             borderWidth = 4;
             marginRight = "-10px";
+            title = "Result"
             break;
 
     }
     return (
-        <IconButton
-            onClick={() => renderIHGraph(property, getNode, fitView)}
-            sx={{
-                marginLeft: marginLeft,
-                marginRight: marginRight,
-            }}
+        <Tooltip
+            placement="top"
+            title={title}
         >
-            <Avatar
-                style={{
-                    backgroundColor: "#e0e0e0",
-                    borderColor: "rgb(97, 97, 97)",
-                    borderStyle: borderStyle,
-                    borderWidth: borderWidth,
-                    color: "rgb(97, 97, 97)",
-                    cursor: "pointer",
+            <IconButton
+                onClick={() => renderIHGraph(property, getNode, fitView)}
+                sx={{
+                    marginLeft: marginLeft,
+                    marginRight: marginRight,
                 }}
             >
-                <AccountTreeIcon color="action"/>
-            </Avatar>
-        </IconButton>
+                <Avatar
+                    style={{
+                        backgroundColor: "#e0e0e0",
+                        borderColor: "rgb(97, 97, 97)",
+                        borderStyle: borderStyle,
+                        borderWidth: borderWidth,
+                        color: "rgb(97, 97, 97)",
+                        cursor: "pointer",
+                    }}
+                >
+                    <AccountTreeIcon color="action"/>
+                </Avatar>
+            </IconButton>
+        </Tooltip>
     );
 }
