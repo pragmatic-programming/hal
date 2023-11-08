cf12d46cd5e5e84a5ae4679b7a25b09232e69223
Steven Smyth <ssm@informatik.uni-kiel.de>
2023-11-08 19:50:00 +0100

    Add error and warning colors to model processor ui bottom component

--- src/ui/bottom/ModelProcessor.tsx
+++ src/ui/bottom/ModelProcessor.tsx
@@ -2,7 +2,7 @@ import { Chip, Theme, useTheme } from "@mui/material";
 import React from "react";
 import "./Bottom.scss";
 import MemoryIcon from "@mui/icons-material/Memory";
-import { Processor } from "../../../../kico-core";
+import { Processor } from "kico";
 
 interface Props {
     processor: Processor<any, any>;
@@ -10,11 +10,23 @@ interface Props {
 
 export default function ModelProcessor(props: Props): React.JSX.Element {
     const theme: Theme = useTheme();
+    let background: string = theme.palette.primary.dark;
+
+    // TODO: color contrasts
+    if (props.processor.getStatus().hasWarnings()) {
+        background = theme.palette.warning.main;
+    }    
+    if (props.processor.getStatus().hasErrors()) {
+        background = theme.palette.error.main;
+    }
+
     return (
         <Chip
             icon={<MemoryIcon/>}
             label={props.processor.getName()}
-            style={{backgroundColor: theme.palette.primary.dark}}
+            style={{
+                backgroundColor: background
+            }}
         />
     );
 }
