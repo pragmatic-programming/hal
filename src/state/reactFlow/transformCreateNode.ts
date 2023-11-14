9de4875e24492da4b33f60529c47facb3403ce78
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-11-14 16:47:20 +0100

    Transform create edge if new node has only one possible source edge type

--- src/state/reactFlow/transformCreateNode.ts
+++ src/state/reactFlow/transformCreateNode.ts
@@ -8,12 +8,14 @@ import { createEdgeDataCreate } from "../../model/edge/createEdgeData";
 import { EdgeTypeIndicator, edgeTypeIndicators } from "../../model/edge/EdgeTypeIndicator";
 import { NodeDefinition } from "../../model/node/NodeDefinition";
 import { NodeTypeIndicator } from "../../model/node/NodeTypeIndicator";
+import { transformCreateEdgeByEdgeDefinition } from "./transformCreateEdge";
+import { retrieveEdgeDefinition } from "../../model/edge/edgeDefinitions";
 
 
 export function transformCreateNode(setState: StoreApi<State>["setState"], getState: () => State) {
     return async (nodeId: string, nodeDefinition: NodeDefinition, targetEdgeId: string | undefined): Promise<void> => {
         const reactFlow: StateReactFlow = getState().reactFlow;
-        const nodes: Node[] = transformNodes(reactFlow, nodeDefinition.type, nodeId);
+        const nodes: Node[] = transformNodes(reactFlow, nodeDefinition, nodeId);
         let edges: Edge[] = reactFlow.edges;
         // if targetEdgeId is not set, no target edge exist and,
         // thus we do not need to transform edges
@@ -30,21 +32,41 @@ export function transformCreateNode(setState: StoreApi<State>["setState"], getSt
     };
 }
 
-// todo function name
-export function transformNodes(reactFlow: StateReactFlow, type: NodeTypeIndicator, nodeId: string): Node[] {
+export function transformCreateNodeByNodeTypeIndicator(node: Node, type: NodeTypeIndicator): Node {
+    if (node.type === "create") {
+        const data: NodeData = createNodeDataFromCreationNode(type);
+        node.type = type;
+        node.data = data;
+        node.height = data.height;
+        node.width = data.width;
+    }
+    return node;
+}
+
+function transformNodes(reactFlow: StateReactFlow, nodeDefinition: NodeDefinition, nodeId: string): Node[] {
     return reactFlow.nodes.map((node: Node) => {
-        if (node.id === nodeId && node.type === "create") {
-            const data: NodeData = createNodeDataFromCreationNode(type);
-            node.type = type;
-            node.data = data;
-            node.height = data.height;
-            node.width = data.width;
+        if (node.id === nodeId) {
+            node = transformCreateNodeByNodeTypeIndicator(node, nodeDefinition.type);
         }
         return node;
     });
 }
 
 function transformEdges(reactFlow: StateReactFlow, nodeDefinition: NodeDefinition, edgeId: string): Edge[] {
+    // if only one sourceEdgeType exist,
+    // we can transform the target edge
+    if (nodeDefinition.sourceEdgeTypes.length === 1) {
+        return reactFlow.edges.map((edge: Edge) => {
+            if (edge.id === edgeId) {
+                const firstSourceEdgeType: EdgeTypeIndicator = nodeDefinition.sourceEdgeTypes[0];
+                edge = transformCreateEdgeByEdgeDefinition(
+                    edge,
+                    retrieveEdgeDefinition(firstSourceEdgeType)
+                );
+            }
+            return edge;
+        });
+    }
     return reactFlow.edges.map((edge: Edge) => {
         if (edge.id === edgeId) {
             if (edge.type !== "create") {
