ca0bb58d8bbd6665adbaa3c59b24c84c61139f80
Tim Tegeler <15845152+fetchandadd@users.noreply.github.com>
2023-11-23 16:04:29 +0100

    Introduce EditorHeaderIcon (#18)
    * Introduce EditorHeaderIcon
    
    * Introduce EditorHeaderIconDynamic and ...Static
    
    * Simplify node background

--- gui/src/ui/editor/EditorHeader.tsx
+++ gui/src/ui/editor/EditorHeader.tsx
@@ -1,50 +1,34 @@
 import React from "react";
-import { InsertDriveFile } from "@mui/icons-material";
-import { useStore } from "../../state/Store";
-import { State } from "../../state/State";
-import { ReactFlowInstance, useReactFlow } from "reactflow";
-import { NodeData } from "../../model/node/NodeData";
-import { BoxBackgroundLight } from "../util/BoxBackgroundLight";
 import { EditorHeaderLabel } from "./EditorHeaderLabel";
-import { strictNode, StrictNode } from "../../model/node/StrictNode";
 
 interface Props {
     nodeId: string,
     value: string | undefined,
     onChange: (content: string) => void,
+    iconLeft: React.JSX.Element
+    iconRight: React.JSX.Element
 }
-
 
 export const editorHeaderHeight = 46;
 
 export default function EditorHeader(props: Props): React.JSX.Element {
-    const openEditor = useStore((state: State) => state.editor.editorOpen);
-    const reactFlow: ReactFlowInstance = useReactFlow();
-    const node: StrictNode<NodeData> = strictNode(reactFlow.getNode(props.nodeId));
-    if (node.data.type !== "editor") {
-        throw new Error("Node has wrong type");
-    }
     return (
-        <BoxBackgroundLight
+        <div
             style={{
                 alignItems: "center",
                 display: "flex",
                 height: editorHeaderHeight,
-                marginRight: 46,
-                paddingLeft: 8,
             }}
         >
-            <InsertDriveFile
-                onClick={() => openEditor(reactFlow.getNode, props.nodeId)}
-                sx={{color: "action.active"}}
-            />
+            {props.iconLeft}
             <EditorHeaderLabel
+                className="nodrag"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value)}
                 size="small"
                 value={props.value}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    props.onChange(event.target.value);
-                }}
+                fullWidth={true}
             />
-        </BoxBackgroundLight>
+            {props.iconRight}
+        </div>
     );
 }
