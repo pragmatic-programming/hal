458fa46a6c7e3bfdd40207d65a6129ae00dce460
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2023-12-12 16:13:00 +0100

    Introduce EdgeCreateLabel and show initially only n-elements

--- gui/src/ui/flow/edge/EdgeCreate.tsx
+++ gui/src/ui/flow/edge/EdgeCreate.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import { BaseEdge, EdgeLabelRenderer, EdgeProps } from "reactflow";
-import { IconButton, SvgIcon, Tooltip } from "@mui/material";
 import { useStore } from "../../../state/Store";
 import { State } from "../../../state/State";
-import { BoxBackgroundMain } from "../../util/BoxBackgroundMain";
-import { Add } from "@mui/icons-material";
-import { edgeDefinitions, retrieveEdgeDefinition } from "../../../model/edge/edgeDefinitions";
+import { retrieveEdgeDefinition } from "../../../model/edge/edgeDefinitions";
 import { EdgeDefinition } from "../../../model/edge/EdgeDefinition";
-import { firstCharUpperCase } from "../../../util";
 import { EdgeDataCreate } from "../../../model/edge/EdgeData";
 import { EdgePath, getEdgePath } from "../../../model/edge/EdgePath";
 import { EdgePathStyle } from "../../../model/edge/EdgePathStyle";
+import EdgeCreateLabel from "./EdgeCreateLabel";
 
 export default function EdgeCreate(props: EdgeProps<EdgeDataCreate>): React.JSX.Element {
     const edgePathStyle: EdgePathStyle = useStore((state: State) => state.flow.edgePathStyle);
     const edgePath: EdgePath = getEdgePath(edgePathStyle, props);
     const edgeDefinition: EdgeDefinition = retrieveEdgeDefinition("create");
-    const transformCreationEdge = useStore((state: State) => state.flow.transformCreateEdge);
     const edgeData: EdgeDataCreate | undefined = props.data;
     if (!edgeData) {
         throw new Error("Props.edgeData is undefined");
@@ -38,37 +34,12 @@ export default function EdgeCreate(props: EdgeProps<EdgeDataCreate>): React.JSX.
                     }}
                     className="nopan nodrag"
                 >
-                    <BoxBackgroundMain>
-                        {edgeDefinitions
-                            .filter((edgeDefinition: EdgeDefinition): boolean => edgeDefinition.type !== "create" && !edgeData.deniedEdgeTypes.includes(edgeDefinition.type))
-                            .map((edgeDefinition: EdgeDefinition) =>
-                                <Tooltip
-                                    key={edgeDefinition.type}
-                                    placement="top"
-                                    title={"Create new " + firstCharUpperCase(edgeDefinition.type) + " Edge"}
-                                >
-                                    <IconButton
-                                        onClick={() => transformCreationEdge(props.id, edgeDefinition, props.target,)}
-                                    >
-                                        <SvgIcon component={edgeDefinition.icon}></SvgIcon>
-                                    </IconButton>
-                                </Tooltip>
-                            )
-                        }
-                        <Tooltip
-                            placement="top"
-                            title={"Open new Edge Dialog"}
-                        >
-                            {/* todo: remove span when IconButton is not disabled anymore */}
-                            <span>
-                                <IconButton
-                                    disabled={true}
-                                >
-                                    <Add/>
-                                </IconButton>
-                            </span>
-                        </Tooltip>
-                    </BoxBackgroundMain>
+                    <EdgeCreateLabel
+                        id={props.id}
+                        deniedEdgeTypes={edgeData.deniedEdgeTypes}
+                        edgePath={edgePath}
+                        targetNodeId={props.target}
+                    />
                 </div>
             </EdgeLabelRenderer>
         </>
