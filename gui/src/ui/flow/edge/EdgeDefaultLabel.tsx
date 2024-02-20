43c1d30e9f04f81a38269cd30bbc66acf87c031c
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2024-02-20 15:24:35 +0100

    Fix NaN bug

--- gui/src/ui/flow/edge/EdgeDefaultLabel.tsx
+++ gui/src/ui/flow/edge/EdgeDefaultLabel.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { InputAdornment, Theme, useTheme } from "@mui/material";
 import { useStore } from "../../../state/Store";
 import { State } from "../../../state/State";
 import { EdgeDefinition } from "../../../model/edge/EdgeDefinition";
-import { EdgeDefaultLabelTextField } from "./EdgeDefaultLabelTextField";
 import { EdgeDefaultLabelCross } from "./EdgeDefaultLabelCross";
-import { EdgeDefaultLabelIcon } from "./EdgeDefaultLabelIcon";
 import { EdgeData } from "../../../model/edge/EdgeData";
+import EdgeDefaultLabelVerbose from "./EdgeDefaultLabelVerbose";
 
 interface Props {
     edgeDefinition: EdgeDefinition;
@@ -16,57 +14,23 @@ interface Props {
 }
 
 export default function EdgeDefaultLabel(props: Props): React.JSX.Element {
-    const setEdgeLabel = useStore((state: State) => state.flow.setEdgeLabel);
-    const setEdgePriority = useStore((state: State) => state.flow.setEdgePriority);
     const verboseMode: boolean = useStore((state: State) => state.flow.verboseMode);
-    const theme: Theme = useTheme();
-    let content: React.JSX.Element = (
+    if (verboseMode) {
+        return (
+            <EdgeDefaultLabelVerbose
+                edgeData={props.edgeData}
+                edgeDefinition={props.edgeDefinition}
+                id={props.id}
+                label={props.label}
+            />
+        );
+    }
+    return (
         <EdgeDefaultLabelCross
             edgeDefinition={props.edgeDefinition}
             edgePathStyle={props.edgeData.edgePathStyle}
             id={props.id}
         />
     );
-    if (verboseMode) {
-        content = (
-            <>
-                <EdgeDefaultLabelTextField
-                    size="small"
-                    InputProps={{
-                        inputProps: {
-                            style: {
-                                textAlign: "center",
-                            }
-                        },
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <EdgeDefaultLabelIcon
-                                    icon={props.edgeDefinition.icon}
-                                    id={props.id}
-                                />
-                            </InputAdornment>
-                        ),
-                    }}
-                    value={props.edgeData.priority}
-                    // todo bug NaN
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEdgePriority(props.id, parseInt(event.target.value))}
-                    style={{
-                        backgroundColor: theme.palette.primary.main,
-                        width: 100,
-                    }}
-                />
-                <EdgeDefaultLabelTextField
-                    size="small"
-                    value={props.label}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEdgeLabel(props.id, event.target.value)}
-                    style={{
-                        backgroundColor: theme.palette.primary.main,
-                        width: Math.max(130, props.label.length * 12),
-                    }}
-                />
-            </>
-        );
-    }
-    return content;
 }
 
---
+++ gui/src/ui/flow/edge/EdgeDefaultLabelVerbose.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { InputAdornment, Theme, useTheme } from "@mui/material";
+import { useStore } from "../../../state/Store";
+import { State } from "../../../state/State";
+import { EdgeDefinition } from "../../../model/edge/EdgeDefinition";
+import { EdgeDefaultLabelTextField } from "./EdgeDefaultLabelTextField";
+import { EdgeDefaultLabelIcon } from "./EdgeDefaultLabelIcon";
+import { EdgeData } from "../../../model/edge/EdgeData";
+
+interface Props {
+    edgeDefinition: EdgeDefinition;
+    edgeData: EdgeData;
+    id: string;
+    label: string;
+}
+
+
+export default function EdgeDefaultLabelVerbose(props: Props): React.JSX.Element {
+    const setEdgeLabel = useStore((state: State) => state.flow.setEdgeLabel);
+    const setEdgePriority = useStore((state: State) => state.flow.setEdgePriority);
+    const [tempPriority, setTempPriority] = useState(props.edgeData.priority.toString());
+    const theme: Theme = useTheme();
+
+    function onBlur(): void {
+        const parsedPriority: number = parseInt(tempPriority);
+        if (isNaN(parsedPriority)) {
+            console.log(parsedPriority);
+            return;
+        }
+        setEdgePriority(props.id, parsedPriority);
+    }
+
+    return (
+        <>
+            <EdgeDefaultLabelTextField
+                size="small"
+                InputProps={{
+                    inputProps: {
+                        style: {
+                            textAlign: "center",
+                        }
+                    },
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <EdgeDefaultLabelIcon
+                                icon={props.edgeDefinition.icon}
+                                id={props.id}
+                            />
+                        </InputAdornment>
+                    ),
+                }}
+                value={tempPriority}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTempPriority(event.target.value)}
+                onBlur={onBlur}
+                style={{
+                    backgroundColor: theme.palette.primary.main,
+                    width: 100,
+                }}
+            />
+            <EdgeDefaultLabelTextField
+                size="small"
+                value={props.label}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEdgeLabel(props.id, event.target.value)}
+                style={{
+                    backgroundColor: theme.palette.primary.main,
+                    width: Math.max(130, props.label.length * 12),
+                }}
+            />
+        </>
+    );
+}
+
