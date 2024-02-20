66210afe0be40066059cd849a1a2e1c347a18e2a
Tim Tegeler <tim.tegeler@tu-dortmund.de>
2024-02-20 15:35:18 +0100

    Make EdgeDefaultLabelVerbose less clunky

--- gui/src/ui/flow/edge/EdgeDefaultLabelVerbose.tsx
+++ gui/src/ui/flow/edge/EdgeDefaultLabelVerbose.tsx
@@ -37,6 +37,7 @@ export default function EdgeDefaultLabelVerbose(props: Props): React.JSX.Element
                     inputProps: {
                         style: {
                             textAlign: "center",
+                            paddingRight: 0,
                         }
                     },
                     startAdornment: (
@@ -53,16 +54,23 @@ export default function EdgeDefaultLabelVerbose(props: Props): React.JSX.Element
                 onBlur={onBlur}
                 style={{
                     backgroundColor: theme.palette.primary.main,
-                    width: 100,
+                    width: 60,
                 }}
             />
             <EdgeDefaultLabelTextField
                 size="small"
+                InputProps={{
+                    inputProps: {
+                        style: {
+                            paddingRight: 0,
+                        }
+                    },
+                }}
                 value={props.label}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEdgeLabel(props.id, event.target.value)}
                 style={{
                     backgroundColor: theme.palette.primary.main,
-                    width: Math.max(130, props.label.length * 12),
+                    width: Math.max(100, props.label.length * 12),
                 }}
             />
         </>
