27eb21d43bd8080df79e25fc6a1585c60a508544
Sebastian Teumert <sebastian.teumert@gmx.de>
2024-05-16 17:26:15 +0200

    Debug prints

--- gui/src/processors/DefaultProcessors.ts
+++ gui/src/processors/DefaultProcessors.ts
@@ -1,11 +1,12 @@
 import { PromptFrameProcessor } from "./edgeTypes/ai/PromptFrameProcessor";
 import { retrieveEdgeDefinition } from "../model/edge/edgeDefinitions";
 import { TransformationProcessor } from "@pragmatic-programming/ihgraph";
-import { Type } from "typescript";
 
 export class DefaultProcessors {
 
     static getProcessor(processorId: string): typeof TransformationProcessor {
+
+        console.log("getProcessor...")
 
         const processors : [string, any][] = (window as any).__additionalProcessors;
         const found = processors.find(item => item[0] === processorId);
@@ -13,8 +14,8 @@ export class DefaultProcessors {
         console.log("Found additional processors: " + processors);
         console.log("Found" + found)
 
-        // if (found !== undefined)
-        //     return found[1];
+        if (found !== undefined)
+            return found[1];
 
         switch (processorId) {
             case "promptframe":
